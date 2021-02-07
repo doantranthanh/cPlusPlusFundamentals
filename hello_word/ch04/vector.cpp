4ec882f62110aa39ce6824fb3e24c3c2fce41bb2
thanhdoan <thanh.doan1@rbi.co.uk>
2021-02-07 14:32:09 +0000

    Thanh |Vector

---
+++ hello_word/ch04/vector.cpp
@@ -0,0 +1,14 @@
+#include "../include/std_lib_facilities.h"
+
+int main()
+{
+    vector<double> v;
+    v.push_back(2.7);
+    v.push_back(5.6);
+    v.push_back(7.9);
+    int size = v.size();
+    for (int i = 0; i < size; ++i)
+    {
+        cout << "v[" << i << "]==" << v[i] << '\n';
+    }
+}
